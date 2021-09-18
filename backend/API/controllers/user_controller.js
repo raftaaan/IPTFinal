c560ea42bd34f1d0c19fbd98a6c3b741d8655b00
jmarknavarro <jmnavarro0903@gmail.com>
2021-09-19 00:37:04 +0800

    s

--- backend/API/controllers/user_controller.js
+++ backend/API/controllers/user_controller.js
@@ -2,17 +2,17 @@ const db = require ('../models/db_connection')
 
 exports.login = (req, res, next) => {
     //1. body
-    let email = req.body.username;
+    let username = req.body.username;
     let password = req.body.password;
 
-    if(email == '' || email == undefined || password == '' || password == undefined ){
+    if(username == '' || username == undefined || password == '' || password == undefined ){
         res.status(500).send({
             successful: false,
             message: "Username and password are required"
         })
     }
     else {
-        let searchQuery = `SELECT * FROM users WHERE username = '${username}'`
+        let searchQuery = `SELECT * FROM accounts WHERE Username = '${username}'`; 
        db.query(searchQuery, (err, rows, result)=>{
            console.log("err==" +err);
            if(err) {
@@ -29,11 +29,12 @@ exports.login = (req, res, next) => {
                     })
                 }
                 else {
-                    let passwordDb = rows[0].password;
+                    let passwordDb = rows[0].Password;
                     if(passwordDb == password) {
                         res.status(200).send({
                             successful: true,
-                            message: "Successful Login"
+                            message: "Successful Login",
+                            status: rows[0].status
                             })
                     }
                     else {
@@ -55,17 +56,27 @@ exports.login = (req, res, next) => {
 }
 
 exports.createUser = (req, res, next) => {
+    let email = req.body.email;
     let username = req.body.username;
     let password = req.body.password;
+    let lastname = req.body.lastname;
+    let middlename = req.body.middlename;
+    let firstname = req.body.firstname;
+    let contactnumber = req.body.contactnumber;
+    let housenumber = req.body.housenumber;
+    let street = req.body.street;
+    let municipality = req.body.municipality;
+    let province = req.body.province;
+    let zipcode = req.body.zipcode;
 
     if(username == '' || username == undefined || password == '' || password == undefined ){
         res.status(500).send({
             successful: false,
-            message: "Username and password are required"
+            message: "Username and Password are required"
         })
     }
     else {
-        let selectUnameQuery = `SELECT username FROM users WHERE username = '${username}'`
+        let selectUnameQuery = `SELECT Username FROM accounts WHERE Username = '${username}'`
 
         db.query(selectUnameQuery, (err, rows, result)=>{
             if(err) {
@@ -78,11 +89,21 @@ exports.createUser = (req, res, next) => {
                 console.log("rows ==" +JSON.stringify(rows));
                 if(rows.length == 0) {
                     //no username existing
-                    let insertUserQuery =`INSERT INTO users SET ?`;
+                    let insertUserQuery =`INSERT INTO accounts SET ?`;
 
                     let insertUserData = {
-                        username : username,
-                        password : password
+                        Email : email,
+                        Username : username,
+                        Password : password,
+                        LastName : lastname,
+                        MiddleName : middlename,
+                        FirstName : firstname,
+                        ContactNumber : contactnumber,
+                        HouseNo : housenumber, 
+                        Street : street,
+                        Municipality : municipality,
+                        Province : province,
+                        ZipCode : zipcode,
                     }
 
                     db.query(insertUserQuery, insertUserData, (err, rows, result) =>{
@@ -154,7 +175,7 @@ exports.updateUser = (req, res, next) => {
         //validate if new username is taken
         //validate if username  is existing in the database
 
-        let selectUsernameQuery = `SELECT username FROM users WHERE username = '${newUsername}'`
+        let selectUsernameQuery = `SELECT Username FROM accounts WHERE Username = '${newUsername}'`
 
         db.query(selectUsernameQuery, (err, rows, result)=>{
             if(err) {
@@ -170,7 +191,7 @@ exports.updateUser = (req, res, next) => {
                     })
             }
             else {
-                let selectNewUsernameQuery = `SELECT username FROM users WHERE username = '${username}'`
+                let selectNewUsernameQuery = `SELECT Username FROM Users WHERE Username = '${username}'`
 
                 db.query(selectNewUsernameQuery, (err, rows, result)=>{
                     if(err) {
@@ -180,7 +201,7 @@ exports.updateUser = (req, res, next) => {
                             })
                     }
                     else if (rows.length == 0){
-                            let updateUserQuery = `UPDATE users SET username = '${newUsername}', password = '${newPassword}' WHERE username = '${username}'`
+                            let updateUserQuery = `UPDATE accounts SET Username = '${newUsername}', Password = '${newPassword}' WHERE Username = '${username}'`
 
                             db.query(updateUserQuery, (err, rows, result)=>{
                                 if(err) {
