7e066b2facc702fc2601ea9c1bef118371ea065b
raftaaan <61544873+raftaaan@users.noreply.github.com>
2021-09-19 21:06:11 +0800

    pakyutangina

--- backend/API/routers/product_router.js
+++ backend/API/routers/product_router.js
@@ -7,3 +7,5 @@ productRouter.post('/add', productController.addProduct);
 productRouter.post('/delete', productController.deleteProduct);
 productRouter.get('/list', productController.getProduct);
 module.exports = productRouter;
+
+sldladkjqnxjaskm
